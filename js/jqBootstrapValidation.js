9401ff12cf4ddde5ddf28f05a05708e99c6d8f45
georgegoodmanproductions <georgegoodmanproductions@gmail.com>
2016-02-29 19:06:26 -0500

    Mobile Redirect 2
    New Jquery attempt

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -10,8 +10,7 @@
 
 if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )     
 {
-   var url = "http://georgegoodmanproductions.github.io/mobile3";    
-   $(location).attr('href',url);
+   window.location = "georgegoodmanproductions.github.io/mobile3";
 
 }
 
